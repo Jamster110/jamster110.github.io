8b18598fee33c322baf876c847b571029e5e2925
Jamster110 <jamsterhughes@gmail.com>
2024-12-09 21:56:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
 }
+document.getElementById('sidebarToggle').addEventListener('click', function () {
+    document.querySelector('.sidebar').classList.toggle('open');
+});
